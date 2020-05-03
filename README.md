05c4ffdb624092ae52fd09484d9878892817e0c2
Tpoppp <olson0002@yahoo.com>
2020-05-02 22:08:07 -0700

    added some functions and template literals

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# Ready-ReadMe
+
+    #### Table
+    * Table of Contents
+    * [Author](#author)
+    * [title](#title)
+    * [description](#description)
+    * [installation](#installation)
+    * Usage
+    * License
+    * Contributing
+    * Tests
+    * Questions
+        * Would you like to add a profile picture?
+        * What is your GitHub email?
+    
+    #### Author
+​
+    The author of this project is: Thomas Olson
+​
+    #### Title
+​
+    undefined
+​
+    #### Decription
+​
+    A functional generqtor tool to create a fast, easy, and organized readMe for any project
+​
+    #### Installation
+​
+    Install the project by doing the following: npm install (inquirer, util. fs)
+​
+    #### Usage
+​
+    Usage: To create a quick readMe for my project or program
+​
+    #### License
+​
+    License: MIT
+​
+    #### Contributing
+​
+    Contributers: none
+​
+    #### Tests
+​
+    Tests: undefined
+​
+    #### Picture
+​
+    
+​
+    #### Email
+​
+    
--- index.js
+++ index.js
@@ -1,86 +1,145 @@
 const inquirer = require("inquirer");
+const fs = require("fs")
+// const axios = require("axios")
+const util = require("util")
 
-inquirer
-.prompt([
-    {
-        type: "input",
-        message: "What would you like to title this project?",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "What is your name?",
-        name: "author"
-    },
-    {
-        type: "input",
-        message: "How would you describe this project?",
-        name: "description"
-    },
-    {
-        type: "input",
-        message: "What installation is needed?",
-        name: "installation"
-    },
-    {
-        type: "input",
-        message: "How would you use your project?",
-        name: "usage"
-    },
-    {
-        type: "list",
-        message: "What license would you like to add?",
-        name: "license",
-        choices: [
-            "MIT",
-            "ISC",
-            "GPL",
-            "None"
-        ]
-    },
-    {
-        type: "input",
-        message: "Any contributors?",
-        name: "contributing"
-    },
-    {
-        type: "confirm",
-        message: "Any testing done?",
-        name: "test"
-    },
-    {
-        type: "input",
-        message: "What is your github email?",
-        name: "questions"
-    },
-    {
-        type: "input",
-        message: "Would you like to add a profile picture?",
-        name: "questions"
-    },
-])
-.then(function(data) {
+const writeFileAsync = util.promisify(fs.writeFile);
 
-    var fileName = data.name.toLowerCase().split(' ').join('') + ".md";
+// const { author, title, description, installation, usage, license, contributing, tests, picture, email, githubUserName } = answers;
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What would you like to title this project?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is your name?",
+            name: "author"
+        },
+        {
+            type: "input",
+            message: "How would you describe this project?",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "What installation is needed?",
+            name: "installation"
+        },
+        {
+            type: "input",
+            message: "How would you use your project?",
+            name: "usage"
+        },
+        {
+            type: "list",
+            message: "What license would you like to add?",
+            name: "license",
+            choices: [
+                "MIT",
+                "ISC",
+                "GPL",
+                "None"
+            ]
+        },
+        {
+            type: "input",
+            message: "Any contributors?",
+            name: "contributing"
+        },
+        {
+            type: "confirm",
+            message: "Any testing done?",
+            name: "test"
+        },
+        {
+            type: "input",
+            message: "What is your github email?",
+            name: "questions"
+        },
+        {
+            type: "input",
+            message: "Would you like to add a profile picture?",
+            name: "questions"
+        },
+    ])
+};
+
+function generateReadMe(answers) {
+    console.log(answers)
+    return `
+    #### Table
+    * Table of Contents
+    * [Author](#author)
+    * [title](#title)
+    * [description](#description)
+    * [installation](#installation)
+    * Usage
+    * License
+    * Contributing
+    * Tests
+    * Questions
+        * Would you like to add a profile picture?
+        * What is your GitHub email?
+    
+    #### Author
+​
+    The author of this project is: ${answers.author}
+​
+    #### Title
+​
+    ${answers.title}
+​
+    #### Decription
+​
+    ${answers.description}
+​
+    #### Installation
+​
+    Install the project by doing the following: ${answers.installation}
+​
+    #### Usage
+​
+    Usage: ${answers.usage}
+​
+    #### License
+​
+    License: ${answers.license}
+​
+    #### Contributing
+​
+    Contributers: ${answers.contributing}
+​
+    #### Tests
+​
+    Tests: ${answers.tests}
+​
+    #### Picture
+​
+    ${answers.picture === "Yes" ? `<img>![profile_image](${answers.avatar})</img>` : ""}
+​
+    #### Email
+​
+    ${answers.email === "Yes" ? `${answers.email}` : ""}`
+}
+    
+
+async function init() {
+    console.log("readMe")
+    try {
+      const answers = await promptUser();
   
-    inquirer.writeToFile(fileName, JSON.stringify(data, null, '\t'), function(err) {
+      const readMe = generateReadMe(answers);
   
-      if (err) {
-        return console.log(err);
-      }
+      await writeFileAsync("readme.md", readMe);
   
-      console.log("Success!");
-  
-    });
-  })
-.catch(err => {
-    if (err) { }
-});
+      console.log("Successfully wrote to readMe.md");
+    } catch(err) {
+      console.log(err);
+    }
+  }
 
-
-
-// function init() {
-
-// }
-
-// init();
+init();
